0e10aefeb8d2e47338fae30dc9df8ca68cab9a0b
Rishabh Chauhan <chauhanrishabh30@gmail.com>
2024-08-07 17:19:00 +0530

    Added the end points and created use-get-account hook

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,8 @@
 import { Header } from "@/components/header";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
+import { QueryProvider } from "@/providers/query-provider";
 type Props = {
   children: React.ReactNode;
 };
@@ -8,7 +11,9 @@ const DashboardLayout = ({ children }: Props) => {
   return (
     <>
       <Header />
-      <main className="px-3 lg:px-14">{children}</main>
+      <main className="px-3 lg:px-14">
+        <QueryProvider>{children}</QueryProvider>
+      </main>
     </>
   );
 };
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,13 +1,25 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { HTTPException } from "hono/http-exception";
+import accounts from "./accounts";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c) => {
-  return c.json({ hello: "World" });
-});
+// app.onError((err, c) => {
+//   if (err instanceof HTTPException) {
+//     return err.getResponse();
+//   }
+//   return c.json({ error: "Internal error" }, 500);
+// });
+// app.get("/hello", (c) => {
+//   return c.json({ hello: "World" });
+// });
+
+const routes = app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
